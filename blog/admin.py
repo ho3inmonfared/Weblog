4c0d6b0b1f61dc1e00ef81d9f81359a80b48f9ce
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-12-16 11:23:55 +0330

    Create Post model and set them on admin panel(step 3)
    I create model and admin panel . I use def short_description for making long dsc to short dsc.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+from django.utils.text import Truncator
 
-# Register your models here.
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','short_description','author','created_at','status', )
+    list_filter=('created_at','status', )
+    search_fields=('title','short_description', )
+    
+    def short_description(self,obj):
+        return Truncator(obj.description).words(5)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    
+    STATUS_CHOICES=(
+        ('pub','publish'),
+        ('drf','draft'),
+    )
+    
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    status=models.CharField(choices=STATUS_CHOICES,default='drf',max_length=100)
+    
+    
+
