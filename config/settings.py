3a007ca8e26f94b109d6345d62969d21207f503c
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-12-24 20:14:30 +0330

    Create app accounts and login & logout (step 11)
    I use django auth urls and next step I create urls.py for signup form

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     
     # local-app
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+LOGIN_REDIRECT_URL='post_list_page'
