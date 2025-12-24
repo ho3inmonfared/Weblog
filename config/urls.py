565947865aee4e53ad5ccd2fa73905d50777b648
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-12-24 20:40:50 +0330

    Create signup form (step 12)
    I create signup page and i use accounts urls.py and class view .

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
